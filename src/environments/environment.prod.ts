819a63630cb5c84587c9dd13d9cbdd66fbe16a8d
Antony J Malakkaran <antonyjm462@gmail.com>
2019-10-21 10:01:24 +0530

    firebase fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBjiZSPBgIuHWoFYNE26KDGZ8ews1xSayI",
+    authDomain: "fixel-project.firebaseapp.com",
+    databaseURL: "https://fixel-project.firebaseio.com",
+    projectId: "fixel-project",
+    storageBucket: "fixel-project.appspot.com",
+    messagingSenderId: "1096335909006",
+    appId: "1:1096335909006:web:3013a54f0a1a635f6429df",
+    measurementId: "G-3GM7P4MMG8"
+  }
 };
