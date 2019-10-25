291b59f349b4b382a90f71adf79d8ac53cde587e
Antony J Malakkaran <antonyjm462@gmail.com>
2019-10-25 17:45:54 +0530

    api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/toPromise';
+import { Upfile } from './model/upfile';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +11,10 @@ import 'rxjs/add/operator/toPromise';
 export class ApiService {
   url : string = 'http://127.0.0.1:8000/';
 
-  constructor(private http : Http) { }
+  constructor(private http1 : Http,private http: HttpClient) { }
 
-  uploadphoto(photo: any): Observable<any>{
-  return this.http.post<any>(`${this.url}`, photo);
+  uploadphoto(photo: Upfile): Observable<Upfile>{
+  return this.http.post<Upfile>(`${this.url}`, photo);
   }
 
 }
